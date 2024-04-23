2ff74c8433f3c8069cc71a59e6219bb295566cc9
Teodor Pytka <teodorpytka@gmail.com>
2024-04-23 09:53:06 +0200

    tmux changes and better setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@
 
 mkdir ~/.config/i3/
 cp ./.config/i3/config ~/.config/i3/config
+
+
+# tmux 
+# clone TPM - Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp ./.tmux.conf ~/.tmux.conf
